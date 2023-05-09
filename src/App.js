a2a6c6aa3baba7a905a70ccc72a3e0c82b8fe262
Fabio Bauleo <fabio.bauleo.developer@gmail.com>
2023-05-09 06:02:04 +0200

    basic structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-
 import './App.css';
+import Nav from './components/Nav';
+import Header from './components/Header';
+import Projects from './components/Projects';
+import Tools from './components/Tools';
+import Blog from './components/Blog';
+import Contact from './components/Contact';
 
 function App() {
   return (
     <div className="App">
-      
+      <Nav /> 
+      <Header />
+      <Projects /> 
+      <Tools />
+      <Blog />
+      <Contact />    
     </div>
   );
 }
