5a7aeaf6733c89218f9a670aa49d8abc34fac4cd
Fabio Bauleo <fabio.bauleo.developer@gmail.com>
2023-05-20 06:29:30 +0200

    added tools with basic styling

--- src/components/Tools.js
+++ src/components/Tools.js
@@ -1,7 +1,18 @@
+import { useLanguage } from "../contexts/LanguageProvider";
+import text from "../text.json";
+
 export default function Tools(){
+    const { language } = useLanguage();
+    let header;
+    switch(language){
+        case "english" : header = text.tools_text.english; break;
+        case "italiano" : header = text.tools_text.italiano; break;
+        case "deutsch" : header = text.tools_text.deutsch; break;
+    }
     return(
-        <div>
-            Tools
+        <div className="tools">
+            <h1>{ header }</h1>
+            <span>Tool1</span><span>Tool2</span><span>Tool3</span>
         </div>
     );
 }
