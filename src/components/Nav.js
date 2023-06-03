8fad1563365ddd3daa312cc7e7e9c32c923d6fa5
Fabio Bauleo <fabio.bauleo.developer@gmail.com>
2023-06-03 11:27:52 +0200

    added react-responsive with a demo in Nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,16 @@
 import { useLanguage } from "../contexts/LanguageProvider";
+import { useMediaQuery } from "react-responsive"
 import DropMenu from "./DropMenu";
 import text from "../text.json";
 
+const Desktop = ({ children }) => {
+    const isDesktop = useMediaQuery({ minWidth: 992 })
+    return isDesktop ? children : null
+}
+const Mobile = ({ children }) => {
+    const isMobile = useMediaQuery({ maxWidth: 767 })
+    return isMobile ? children : null
+}
 
 export default function Nav(){
     const { language } = useLanguage();
@@ -13,14 +22,17 @@ export default function Nav(){
     }
     return(
         <nav id="navbar">
-            <DropMenu />
-            <div className="links">
-                <h3><a href="#header">{links[0]}</a></h3>
-                <h3><a href="#projects">{links[1]}</a></h3>
-                <h3><a href="#tools">{links[2]}</a></h3>
-                <h3><a href="#about">{links[3]}</a></h3>
-                <h3><a href="#contact">{links[4]}</a></h3>
-            </div>
+            <Desktop>
+                <DropMenu />
+                <div className="links">
+                    <h3><a href="#header">{links[0]}</a></h3>
+                    <h3><a href="#projects">{links[1]}</a></h3>
+                    <h3><a href="#tools">{links[2]}</a></h3>
+                    <h3><a href="#about">{links[3]}</a></h3>
+                    <h3><a href="#contact">{links[4]}</a></h3>
+                </div>
+            </Desktop>
+            <Mobile>Mobile</Mobile>
         </nav>
     );
 }
