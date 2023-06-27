3c9e0fef9060753923edd1e5ccc30473a5415423
Fabio Bauleo <fabio.bauleo.developer@gmail.com>
2023-06-27 19:39:45 +0200

    about page done

--- src/components/About.js
+++ src/components/About.js
@@ -3,16 +3,17 @@ import data from "../text.json";
 
 export default function About(){
     const { language } = useLanguage();
-    let text = [];
+    let header = [];
+    let body;
     switch(language){
-        case "english" : text = data.about_text.header.english; break;
-        case "italiano" : text = data.about_text.header.italiano; break;
-        case "deutsch" : text = data.about_text.header.deutsch; break;
+        case "english" : header = data.about_text.header.english; body = data.about_text.body.english; break;
+        case "italiano" : header = data.about_text.header.italiano; body = data.about_text.body.italiano; break;
+        case "deutsch" : header = data.about_text.header.deutsch; body = data.about_text.body.deutsch; break;
     }
     return(
         <div id="about">
-            <h1>{ text[0] }<span style={{color: "#3AA2C2"}}>{ text [1] }</span></h1>
-            <p></p>
+            <h1>{ header[0] }<span style={{color: "#3AA2C2"}}>{ header [1] }</span></h1>
+            <p>{ body[0] }<span style={{color: "#3AA2C2"}}>{ body[1] }</span>{ body[2] }<span style={{color: "#3AA2C2"}}>{ body[3] }</span>{ body[4] } </p>
         </div>
     );
 }
