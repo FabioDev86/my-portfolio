e496dfb423fd706f7c86db5cc76655f14800997d
Fabio Bauleo <fabio.bauleo.developer@gmail.com>
2023-06-29 06:07:01 +0200

    added alert after submit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,15 +9,19 @@ export default function Contact(){
     const { language } = useLanguage();
     let text = [];
     let errors = [];
+    let alert_msg;
     switch(language){
         case "english" :    text = [data.contacts_text.header.english, data.contacts_text.name_label.english, data.contacts_text.email_label.english, data.contacts_text.message_label.english ]; 
                             errors = [data.contacts_text.required_name.english, data.contacts_text.required_email.english, data.contacts_text.required_message.english, data.contacts_text.invalid_email.english];
+                            alert_msg = data.contacts_text.alert_msg.english;
                             break;
         case "italiano" :   text = [data.contacts_text.header.italiano, data.contacts_text.name_label.italiano, data.contacts_text.email_label.italiano, data.contacts_text.message_label.italiano]; 
                             errors = [data.contacts_text.required_name.italiano, data.contacts_text.required_email.italiano, data.contacts_text.required_message.italiano, data.contacts_text.invalid_email.italiano];
+                            alert_msg = data.contacts_text.alert_msg.italiano;
                             break;
         case "deutsch" :    text = [data.contacts_text.header.deutsch, data.contacts_text.name_label.deutsch, data.contacts_text.email_label.deutsch, data.contacts_text.message_label.deutsch];
                             errors = [data.contacts_text.required_name.deutsch, data.contacts_text.required_email.deutsch, data.contacts_text.required_message.deutsch, data.contacts_text.invalid_email.deutsch];
+                            alert_msg = data.contacts_text.alert_msg.deutch;
                             break;
     }
 
@@ -51,6 +55,7 @@ export default function Contact(){
         emailjs.send('service_wsxg3dy', 'template_59d3ypj', params, 'n629mPkgzuzLZfXwH')
             .then((result) => {
                 console.log(result.text);
+                alert(alert_msg);
             }, (error) => {
                 console.log(error.text);
             }
