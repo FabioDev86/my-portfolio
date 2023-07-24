b28990d6457b7b7a7a85daceed60138fbd23ddc3
Fabio Bauleo <fabio.bauleo.developer@gmail.com>
2023-07-24 05:47:35 +0200

    added traslation to button text

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -11,15 +11,15 @@ export default function Contact(){
     let errors = [];
     let alert_msg;
     switch(language){
-        case "english" :    text = [data.contacts_text.header.english, data.contacts_text.name_label.english, data.contacts_text.email_label.english, data.contacts_text.message_label.english ]; 
+        case "english" :    text = [data.contacts_text.header.english, data.contacts_text.name_label.english, data.contacts_text.email_label.english, data.contacts_text.message_label.english, data.contacts_text.button.english ]; 
                             errors = [data.contacts_text.required_name.english, data.contacts_text.required_email.english, data.contacts_text.required_message.english, data.contacts_text.invalid_email.english];
                             alert_msg = data.contacts_text.alert_msg.english;
                             break;
-        case "italiano" :   text = [data.contacts_text.header.italiano, data.contacts_text.name_label.italiano, data.contacts_text.email_label.italiano, data.contacts_text.message_label.italiano]; 
+        case "italiano" :   text = [data.contacts_text.header.italiano, data.contacts_text.name_label.italiano, data.contacts_text.email_label.italiano, data.contacts_text.message_label.italiano, data.contacts_text.button.italiano]; 
                             errors = [data.contacts_text.required_name.italiano, data.contacts_text.required_email.italiano, data.contacts_text.required_message.italiano, data.contacts_text.invalid_email.italiano];
                             alert_msg = data.contacts_text.alert_msg.italiano;
                             break;
-        case "deutsch" :    text = [data.contacts_text.header.deutsch, data.contacts_text.name_label.deutsch, data.contacts_text.email_label.deutsch, data.contacts_text.message_label.deutsch];
+        case "deutsch" :    text = [data.contacts_text.header.deutsch, data.contacts_text.name_label.deutsch, data.contacts_text.email_label.deutsch, data.contacts_text.message_label.deutsch, data.contacts_text.button.deutch];
                             errors = [data.contacts_text.required_name.deutsch, data.contacts_text.required_email.deutsch, data.contacts_text.required_message.deutsch, data.contacts_text.invalid_email.deutsch];
                             alert_msg = data.contacts_text.alert_msg.deutch;
                             break;
@@ -98,7 +98,7 @@ export default function Contact(){
                         name="message"
                         type="textarea"
                     />
-                    <button type="submit" id="submit">Submit</button>
+                    <button type="submit" id="submit">{text[4]}</button>
                 </Form>
                 
              </Formik>
